960ed3f06052349e3537d560efb5434727335278
Linus <linus-dev@users.noreply.github.com>
2016-07-12 03:57:47 +0200

    Update check.js

--- update/check.js
+++ update/check.js
@@ -1,13 +1,13 @@
 module.exports = {
   updateList: function () {
-	  var allCreeps = Room.find(FIND_MY_CREEPS);
-	  for (i = 0; i < allCreeps; i++) {
-	    switch (allCreeps[i]) {
+    var allCreeps = Room.find(FIND_MY_CREEPS);
+    for (i = 0; i < allCreeps; i++) {
+      switch (allCreeps[i]) {
         case 'HarvBotty': {
           break;
         }
-	    }
-	  }
+      }
+    }
   }	  
   checkDead: function () {
   
